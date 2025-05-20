6f6b2277fcf763e9bb2f0e64733c0c6ec9540090
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2025-05-20 17:35:06 +0200

    Move FormPositionTextField to RoboKit

--- RoboKit Demo/Views/Socket View/Subviews/FormPositionView.swift
+++ RoboKit Demo/Views/Socket View/Subviews/FormPositionView.swift
@@ -28,69 +28,11 @@ public struct FormPositionView: View {
                 
                 VStack(spacing: -10) {
                     ForEach(FormAxis.allCases, id: \.self) { axis in
-                        FormPositionTextField(formAxis: axis)
+                        RoboKit.FormPositionTextField(formAxis: axis)
                             .frame(width: 220)
                     }
                 }
             }
-        }
-    }
-}
-
-public struct FormPositionTextField: View {
-    @Environment(FormManager.self) private var formManager: FormManager
-    let formAxis: RoboKit.FormAxis
-    
-    public init(formAxis: FormAxis) {
-        self.formAxis = formAxis
-    }
-    
-    private var positionValue: Binding<Float> {
-        Binding(
-            get: {
-                switch formAxis {
-                case .lateral:
-                    formManager.formPositionRelativeToRoot.x
-                case .longitudinal:
-                    formManager.formPositionRelativeToRoot.y
-                case .vertical:
-                    formManager.formPositionRelativeToRoot.z
-                }
-            },
-            set: {
-                switch formAxis {
-                case .lateral:
-                    formManager.formPositionRelativeToRoot.x = $0
-                case .longitudinal:
-                    formManager.formPositionRelativeToRoot.y = $0
-                case .vertical:
-                    formManager.formPositionRelativeToRoot.z = $0
-                }
-            }
-        )
-    }
-    
-    private var formAxisLabel: String {
-        switch formAxis {
-        case .lateral: return "X"
-        case .longitudinal: return "Y"
-        case .vertical: return "Z"
-        }
-    }
-    
-    public var body: some View {
-        HStack {
-            Text("\(formAxisLabel)")
-            
-            TextField("Position \(formAxisLabel)", value: positionValue, format: .number)
-                .keyboardType(.numbersAndPunctuation)
-                .font(.system(size: 20))
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 12)
-                        .fill(.regularMaterial)
-                        .padding(12)
-                )
         }
     }
 }
